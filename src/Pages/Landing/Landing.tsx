6a868b68379242c6af5496b358ad0ba0cbd0a88d
Joao Fernandes Lopes <vaiojoao@gmail.com>
2022-12-01 23:12:10 +0100

    GROS PUSH

--- src/Pages/Landing/Landing.tsx
+++ src/Pages/Landing/Landing.tsx
@@ -1,14 +1,21 @@
 import './Landing.scss';
 
-import { SpinLogo } from 'Components';
-
-export const Landing = ()=>{
-    return(
+export const Landing = () => {
+    return (
         <div className="landing">
-            <SpinLogo/>
-            <p>
-                React Boilerplate
-            </p>
+            <div className='sex-info-box'>
+                <h1>SexeInfo</h1>
+                <p>Cette plateforme est dédiée à la sensibilisation, la normalisation, et le partage autour de l’éducation sexuelle, et 
+                      des maladies sexuellement transmissibles.</p>
+                <p>
+                      Vous y trouverez un quiz pour tester vos connaissances dans différents sujets liés 
+                      à la sexualité, une chatroom anonyme pour aborder les sujets cités 
+                      précédemment avec d’autres utilisateurs, ou avec notre 
+                      intelligence artificielle (OpenIA). </p>
+            </div>
+            <div className='quiz-bubble'>
+                <img src={require('./Assets/QuizOrb.png')} alt="QuizzBulle"/>
+            </div>
         </div>
     );
 };
