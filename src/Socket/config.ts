b0f1d3c04d72a68cbae775e7084e69b847cebeb5
Timothe <timothe@lempire.co>
2022-12-02 03:58:00 +0100

    update

--- src/Socket/config.ts
+++ src/Socket/config.ts
@@ -1,8 +1,3 @@
 import { io } from 'socket.io-client';
 
-export const socket = io(process.env.REACT_APP_SOCKET_URL, {
-    withCredentials: true,
-    extraHeaders: {
-        'my-custom-header': 'abcd',
-    },
-});
+export const socket = io('https://websocket-ndi-2022.joaofernandes27.repl.co/');
